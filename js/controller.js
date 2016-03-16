7fd4fb66a927231c684ebb2bf8aaf918f3dddbf1
joeyaza <JAzadehdel@gmail.com>
2016-03-16 09:18:53 +0000

    SLIGHT UPDATE

--- js/controller.js
+++ js/controller.js
@@ -12,7 +12,7 @@ function MainController($http){
   self.target = "";
   self.amount = "";
   self.choice = "eur";
-  self.currencies = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS"]
+  self.currencies = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS","INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"]
   //self.searchCurr = searchCurr;
 
   self.searchCurr = function() {
